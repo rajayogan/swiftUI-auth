7282f5acb796ab14812c337ac090255d6f2e8fee
Rajayogan <rajayogan27@gmail.com>
2025-05-27 01:06:51 +0530

    Initial Commit

---
+++ authswift/authswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  authswiftApp.swift
+//  authswift
+//
+//  Created by Rajayogan on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct authswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ authswiftTests/authswiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  authswiftTests.swift
+//  authswiftTests
+//
+//  Created by Rajayogan on 27/05/25.
+//
+
+import Testing
+@testable import authswift
+
+struct authswiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
