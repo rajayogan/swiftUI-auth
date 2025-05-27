5266ed4618542f1e2efde2c14ce61f16e342f9cf
Rajayogan <rajayogan27@gmail.com>
2025-05-28 00:28:03 +0530

    Initial commit

---
+++ authswift/services/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  authswift
+//
+//  Created by Rajayogan on 27/05/25.
+//
+
+import Foundation
---
+++ authswift/views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  authswift
+//
+//  Created by Rajayogan on 27/05/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
